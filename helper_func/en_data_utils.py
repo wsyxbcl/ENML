1e733f45009e8da516248b00f3a2e38f9ba6c02e
yx_chai@whu.edu.cn <yx_chai@whu.edu.cn>
2017-11-14 20:29:35 +0800

    baseline function added

--- helper_func/en_data_utils.py
+++ helper_func/en_data_utils.py
@@ -9,6 +9,7 @@ import matplotlib
 # Force matplotlib to not use any Xwindows backend.
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+import peakutils
 
 # Convert the experiment data to the training set.
 # The script for data preprocess.
@@ -56,6 +57,18 @@ def load_dataset(dataset_dir, test_ratio=0.3):
     x_train, x_test, y_train, y_test = train_test_split(x_orig, y_orig, test_size=test_ratio)
     return x_train, y_train, x_test, y_test, coordinates
 
+def remove_baseline(x, degree=3):
+    """
+    Use the baseline function from peakutils to remove base line of x. which 
+    iteratively performs a polynomial fitting.
+    
+    deg (int (default: 3))
+    – Degree of the polynomial that will estimate the data baseline
+    """
+    baseline_values = peakutils.baseline(x, deg=degree)
+    x_removed = x - baseline_values
+    return baseline_values, x_removed
+
 def plot_dataset(X, Y, coordinates, save_dir):
     """
     Get n*m dimensional X, plot them to given coordinates
@@ -69,16 +82,26 @@ def plot_dataset(X, Y, coordinates, save_dir):
         ax.set_ylabel('Current/A')
         ax.legend(loc='best')
     plt.savefig(get_save_path(save_dir, 'test.png'), dpi=300)
+    plt.clf()
 
 if __name__ == '__main__':
     # dataset_dir = '/mnt/t/college/last/finaldesign/ENML/code/test/'
-    dataset_dir = 'T:/college/last/finaldesign/ENML/code/test/20171113_test_64'
+    dataset_dir = 'T:/college/last/finaldesign/ENML/code/test/baseline'
     # dataset_dir = 'T:/college/last/finaldesign/ENML/code/test/20171112_test'
     get_dataset(dataset_dir, range(7000, 8000))
     # get_dataset(dataset_dir, range(7936, 8000))
-    x_train, y_train, x_test, y_test, coordinates = load_dataset(dataset_dir+'/dataset')
-    train_x_set = x_train - np.mean(x_train, axis=1).reshape(np.shape(x_train)[0], 1)
-    train_y_set = y_train
-    test_x_set = x_test - np.mean(x_test, axis=1).reshape(np.shape(x_test)[0], 1)
-    test_y_set = y_test
-    # plot_dataset(train_x_set, train_y_set, coordinates, dataset_dir+'/plot')
+    train_x_set, train_y_set, test_x_set, test_y_set, coordinates = load_dataset(dataset_dir+'/dataset')
+    train_x_set = train_x_set - np.mean(train_x_set, axis=1).reshape(np.shape(train_x_set)[0], 1)
+    test_x_set = test_x_set - np.mean(test_x_set, axis=1).reshape(np.shape(test_x_set)[0], 1)
+    # plot_dataset(test_x_set, test_y_set, coordinates, dataset_dir+'/plot')
+
+    # Test baseline function here
+    baseline_values, x_removed = remove_baseline(test_x_set[0], degree=3)
+    x_removed = x_removed - np.mean(x_removed)
+    fig = plt.figure()
+    ax = fig.add_subplot(1,1,1)
+    ax.plot(coordinates, baseline_values, label='baseline')
+    ax.plot(coordinates, test_x_set[0], label='raw data')
+    ax.plot(coordinates, x_removed, label='baseline removed')
+    ax.legend(loc='best')
+    plt.savefig(get_save_path(dataset_dir+'/baseline', 'baseline.png'), dpi=300)
