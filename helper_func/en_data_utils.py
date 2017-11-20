a29e11e9d0100639987882c312e83b71bd9b45ef
wsyxbcl <yx_chai@whu.edu.cn>
2017-11-20 11:05:21 +0800

    FFT function added

--- helper_func/en_data_utils.py
+++ helper_func/en_data_utils.py
@@ -31,6 +31,7 @@ def get_dataset(files_dir, x_range):
     coordinates = []
     # import csv data
     for filename, subdir in walker(files_dir, re.compile('training(.*?)_\d+.csv')):
+        print(subdir+filename)
         y = re.findall('training(\d+)_\d+.csv', filename)[-1]
         # y_one_hot = np.eye(9, dtype=int)[int(y) - 1]
         Y.append(int(y) - 1)
@@ -94,35 +95,60 @@ def plot_dataset(X, Y, coordinates, save_dir, filename):
     plt.savefig(get_save_path(save_dir, filename), dpi=300)
     plt.clf()
 
+def fft(X, coordinates):
+    n = X.shape[1]
+    freq = np.copy(coordinates)
+    # Convert ms to s
+    timestep = (coordinates_np[0,1]-coordinates_np[0,0]) * 0.001
+    X_fft = np.fft.fft(X, axis=1)
+    freq[:] = np.fft.fftfreq(n, d=timestep)
+    return X_fft, freq
+
 if __name__ == '__main__':
-    dataset_dir = '/mnt/t/college/last/finaldesign/ENML/code/test/20171116_8_500'
+    dataset_dir = '/mnt/t/college/last/finaldesign/ENML/code/test/FFTfreq'
     # dataset_dir = 'T:/college/last/finaldesign/ENML/code/test/baseline'
     # dataset_dir = 'T:/college/last/finaldesign/ENML/code/test/20171115_test'
 
-    # TODO combine slice and cropping in one function
-    X_np_1, Y_np_1, coordinates_np_1 = get_dataset(dataset_dir, range(4000, 4500))
-    X_np_2, Y_np_2, coordinates_np_2 = get_dataset(dataset_dir, range(4500, 5000))
-    X_np_3, Y_np_3, coordinates_np_3 = get_dataset(dataset_dir, range(5000, 5500))
-    X_np_4, Y_np_4, coordinates_np_4 = get_dataset(dataset_dir, range(5500, 6000))
-    X_np_5, Y_np_5, coordinates_np_5 = get_dataset(dataset_dir, range(6000, 6500))
-    X_np_6, Y_np_6, coordinates_np_6 = get_dataset(dataset_dir, range(6500, 7000))
-    X_np_7, Y_np_7, coordinates_np_7 = get_dataset(dataset_dir, range(7000, 7500))
-    X_np_8, Y_np_8, coordinates_np_8 = get_dataset(dataset_dir, range(7500, 8000))
+    # TODO combine slice and cropping in one function, seperate cropped data and
+    # then add it to the train set?
+    # Maybe create a larger slice at first, then 3crop the training set but only
+    # 1crop the dev/test set.
+    # X_np_1, Y_np_1, coordinates_np_1 = get_dataset(dataset_dir, range(4000, 4500))
+    # X_np_2, Y_np_2, coordinates_np_2 = get_dataset(dataset_dir, range(4500, 5000))
+    # X_np_3, Y_np_3, coordinates_np_3 = get_dataset(dataset_dir, range(5000, 5500))
+    # X_np_4, Y_np_4, coordinates_np_4 = get_dataset(dataset_dir, range(5500, 6000))
+    # X_np_5, Y_np_5, coordinates_np_5 = get_dataset(dataset_dir, range(6000, 6500))
+    # X_np_6, Y_np_6, coordinates_np_6 = get_dataset(dataset_dir, range(6500, 7000))
+    # X_np_7, Y_np_7, coordinates_np_7 = get_dataset(dataset_dir, range(7000, 7500))
+    # X_np_8, Y_np_8, coordinates_np_8 = get_dataset(dataset_dir, range(7500, 8000))
 
-    save_dataset(np.concatenate((X_np_1, X_np_2, X_np_3, X_np_4, X_np_5, X_np_6, X_np_7, X_np_8)),
-                 np.concatenate((Y_np_1, Y_np_2, Y_np_3, Y_np_4, Y_np_5, Y_np_6, Y_np_7, Y_np_8)),
-                 np.concatenate((coordinates_np_1, coordinates_np_2, coordinates_np_3, coordinates_np_4, coordinates_np_5, coordinates_np_6, coordinates_np_7, coordinates_np_8)),
-                 dataset_dir+'/'+'dataset')
+    # FFT test module
+    X_np, Y_np, coordinates_np = get_dataset(dataset_dir, range(4000, 8000))
+    plot_dataset(X_np, Y_np, coordinates_np, dataset_dir+'/plot', 'X_orig')
+    X_baseline_removed = np.copy(X_np)
+    for i in range(X_np.shape[0]):
+        baseline_values, X_baseline_removed[i] = remove_baseline(X_np[i], degree=1)
+    plot_dataset(X_baseline_removed, Y_np, coordinates_np, dataset_dir+'/plot', 'X_baseline_removed')
+    X_norm = np.copy(X_baseline_removed)
+    X_norm = X_baseline_removed - np.mean(X_baseline_removed, axis=1).reshape(np.shape(X_baseline_removed)[0], 1)
+    plot_dataset(X_norm, Y_np, coordinates_np, dataset_dir+'/plot', 'X_norm')
+    X_fft, freq = fft(X_norm, coordinates_np)
+    plot_dataset(np.abs(X_fft[:, :2000]), Y_np, freq[:, :2000], dataset_dir+'/plot', 'X_FFT')
 
-    train_x_set, train_y_set, test_x_set, test_y_set, coordinates = load_dataset(dataset_dir+'/dataset')
-    print("Shape of train_x_set:")
-    print(train_x_set.shape)
-    print("Shape of test_x_set:")
-    print(test_x_set.shape)
-    print("Shape of train_y_set:")
-    print(train_y_set.shape)
-    print("Shape of test_y_set:")
-    print(test_y_set.shape)
+    # save_dataset(np.concatenate((X_np_1, X_np_2, X_np_3, X_np_4, X_np_5, X_np_6, X_np_7, X_np_8)),
+    #              np.concatenate((Y_np_1, Y_np_2, Y_np_3, Y_np_4, Y_np_5, Y_np_6, Y_np_7, Y_np_8)),
+    #              np.concatenate((coordinates_np_1, coordinates_np_2, coordinates_np_3, coordinates_np_4, coordinates_np_5, coordinates_np_6, coordinates_np_7, coordinates_np_8)),
+    #              dataset_dir+'/'+'dataset')
+
+    # train_x_set, train_y_set, test_x_set, test_y_set, coordinates = load_dataset(dataset_dir+'/dataset')
+    # print("Shape of train_x_set:")
+    # print(train_x_set.shape)
+    # print("Shape of test_x_set:")
+    # print(test_x_set.shape)
+    # print("Shape of train_y_set:")
+    # print(train_y_set.shape)
+    # print("Shape of test_y_set:")
+    # print(test_y_set.shape)
     # plot_dataset(test_x_set, test_y_set, coordinates, dataset_dir+'/plot', 'test_x_set')
     # plot_dataset(train_x_set, train_y_set, coordinates, dataset_dir+'/plot', 'train_x_set')
     
