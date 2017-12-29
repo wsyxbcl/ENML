3f1ae52dcba16acc4fefb0b317ad8642ffc990b3
yx_chai@whu.edu.cn <yx_chai@whu.edu.cn>
2017-12-29 18:53:45 +0800

    finish energylab function

--- helper_func/get_current_time.py
+++ helper_func/get_current_time.py
@@ -3,6 +3,12 @@ import re
 import csv
 from dir_walker import walker
 
+def get_save_path(save_dir, filename):
+    if not os.path.exists(save_dir):
+        os.makedirs(save_dir)
+    path = save_dir+'/'+filename
+    return path
+
 def get_i_t_ivium(source_file, output_file):
     """
     Get current and time information from the fucking weird ivium output
@@ -25,43 +31,61 @@ def get_i_t_ivium(source_file, output_file):
                     f.write('\n')
         print('Converted finished. Saved to '+output_file)
 
-def get_i_t_energylab(source_file, loop_period, num_loop):
+def get_i_t_energylab(dir, filename, loop_period, num_loop):
     """
     Seperate each loop from Energylab output files, each file contains all loops.
     Extract current and time from each loop and rewrite to a csv file.
     """
+    source_file = dir+'/'+filename
     with open(source_file, 'rt') as f:
         reader = csv.reader(f)
         data_list = list(reader)
-        time_list = list(map(float, [row[0] for row in data_list][4:]))
-        current_list = list(map(float, [row[2] for row in data_list][4:]))
+        time_list = list(map(float, [row[0] for row in data_list[4:]]))
+        current_list = list(map(float, [row[2] for row in data_list[4:]]))
+        voltage_list = list(map(float, [row[1] for row in data_list[4:]]))
     time0 = time_list[0]
+    coordinate = list(range(8000))
     for loop in range(num_loop):
         time_write = []
         current_write = []
+        voltage_write = []
         for i, time in enumerate(time_list):
             if time >= (time0 + loop * loop_period) and time < (time0 + (loop + 1) * loop_period):
                 time_write.append(time)
                 current_write.append(current_list[i])
-        with open(source_file[:-4]+'_'+str(loop + 1)+'.csv', 'w') as f:
-            for i, time in enumerate(time_write):
-                f.write(str(time))
+                voltage_write.append(voltage_list[i])
+        with open(get_save_path(dir+'/i_t_csv', filename[:-4]+'_'+str(loop + 1)+'.csv'), 'w') as f:
+            f.write('time/s,I/A')
+            f.write('\n')
+            for i, current in enumerate(current_write):
+                f.write(str(coordinate[i]))
                 f.write(',')
-                f.write(str(current_write[i]))
+                f.write(str(current))
+                f.write('\n')
+        with open(get_save_path(dir+'/e_t_csv', filename[:-4]+'_'+str(loop + 1)+'.csv'), 'w') as f:
+            f.write('time/s,voltage/V')
+            f.write('\n')
+            for i, voltage in enumerate(voltage_write):
+                f.write(str(coordinate[i]))
+                f.write(',')
+                f.write(str(voltage))
                 f.write('\n')
 
 
 if __name__ == '__main__':
     # rootdir = '/mnt/t/college/last/finaldesign/ENML/data/CA_ascii/20171108_p_curve'
-    rootdir = 'T:/college/last/finaldesign/ENML/data/CA_ascii/20171108_p_curve'
-    ivium = 1
+    rootdir = 'T:/college/last/finaldesign/ENML/data/CA_ascii/20171228/energylab'
+    ivium = 0
     count = 0
     # for filename, subdir in walker(rootdir, re.compile('training(.*?)(\d+)$')):
+    # Things for re: first time this works, but the second time, generated files included,
+    # and it fails.
     for filename, subdir in walker(rootdir):
         print('Opening '+filename)
         if ivium:
             get_i_t_ivium(subdir+'/'+filename, subdir+'/'+filename+'.csv')
         else:
-            get_i_t_energylab(subdir+'/'+filename, loop_period=8, num_loop=30)
+            get_i_t_energylab(subdir, filename, loop_period=8, num_loop=30)
         count += 1
+
     print('#Files :%d'%count)
